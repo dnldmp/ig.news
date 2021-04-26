c3d46eb69fbf4ccdbc63e51c699ea60e57ed1911
Danilo Dominoni <danilodomp@gmail.com>
2021-04-26 11:45:28 -0300

    bring all posts from prismic to page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,20 @@ import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic'
 import styles from './styles.module.scss'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
-export default function Posts(){
+type Post = {
+  slug: string;
+  title: string;
+  updatedAt: string;
+  excerpt: string;
+} 
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps){
   return(
     <>
       <Head>
@@ -12,36 +24,13 @@ export default function Posts(){
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Titulo</strong>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, 
-              laborum delectus amet nam fugiat non suscipit sequi dicta eius 
-              maxime, sunt repellendus facilis dolorum tempora voluptates autem 
-              dolor ratione vitae.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Titulo</strong>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, 
-              laborum delectus amet nam fugiat non suscipit sequi dicta eius 
-              maxime, sunt repellendus facilis dolorum tempora voluptates autem 
-              dolor ratione vitae.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Titulo</strong>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, 
-              laborum delectus amet nam fugiat non suscipit sequi dicta eius 
-              maxime, sunt repellendus facilis dolorum tempora voluptates autem 
-              dolor ratione vitae.
-            </p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          )) }
         </div>
       </main>
     </>
@@ -53,9 +42,28 @@ export const getStaticProps: GetStaticProps = async () => {
   const response = await prismic.query([
     Prismic.predicates.at('document.type', 'post')
   ], {
-    fetch: ['post.title', 'post.content']
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100
   })
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      }),
+      excerpt: post.data.content.find(
+        content => content.type === 'paragraph'
+      )?.text ?? ''
+    }
+  })
+  
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
