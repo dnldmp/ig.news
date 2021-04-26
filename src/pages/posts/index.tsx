d3a3cbcf48cfaed79302609ac1d4304436b9e29c
Danilo Dominoni <danilodomp@gmail.com>
2021-04-26 11:12:16 -0300

    add prismic

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,61 @@
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import { getPrismicClient } from '../../services/prismic'
+import styles from './styles.module.scss'
+import Prismic from '@prismicio/client'
+
+export default function Posts(){
+  return(
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Titulo</strong>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, 
+              laborum delectus amet nam fugiat non suscipit sequi dicta eius 
+              maxime, sunt repellendus facilis dolorum tempora voluptates autem 
+              dolor ratione vitae.
+            </p>
+          </a>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Titulo</strong>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, 
+              laborum delectus amet nam fugiat non suscipit sequi dicta eius 
+              maxime, sunt repellendus facilis dolorum tempora voluptates autem 
+              dolor ratione vitae.
+            </p>
+          </a>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Titulo</strong>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit, 
+              laborum delectus amet nam fugiat non suscipit sequi dicta eius 
+              maxime, sunt repellendus facilis dolorum tempora voluptates autem 
+              dolor ratione vitae.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')
+  ], {
+    fetch: ['post.title', 'post.content']
+  })
+  return {
+    props: {}
+  }
+}
